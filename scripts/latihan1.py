1b6902bf0a4f2b3335284bcac6371a1cce8a0138
Ali Akbar Septiandri <mail.septiandri@gmail.com>
2017-11-27 06:29:28 +0700

    Latihan 1

---
+++ scripts/latihan1.py
@@ -0,0 +1,53 @@
+# A. match_ends
+# Diberikan list of strings, kembalikan jumlah strings
+# dengan panjang 2 atau lebih DAN karakter pertama dan terakhirnya sama.
+def match_ends(words):
+    # Kode Anda di sini
+    return
+
+# B. manhattan_distance
+# Diberikan dua tuple sebagai representasi titik dalam n dimensi
+# hitung jarak antara kedua titik tersebut dengan metode Manhattan distance
+# Contoh: jarak (2, 3) dan (5, 4) adalah |2-5| + |3-4| = |-3| + |-1| = 4
+#         jarak (9, 5, 3) dan (1, 2, 1) adalah |9-1| + |5-2| + |3-1| = 8 + 3 + 2 = 13
+def manhattan_distance(p1, p2):
+    # Kode Anda di sini
+    return
+
+# C. triangle
+# Diberikan integer n, cetak segitiga dengan 'tinggi' n dan 'alas' n
+# Contoh: triangle(3)
+#         *
+#         **
+#         ***
+#         triangle(5)
+#         *
+#         **
+#         ***
+#         ****
+#         *****
+# Variasi: Bagaimana kalau segitiganya dibalik?
+# Contoh: triangle(3)
+#           *
+#          **
+#         ***
+def triangle(n):
+    # Kode Anda di sini
+
+# D. power
+# Diberikan dua integer a dan b,
+# hitung nilai a^b
+# Catatan: Anda tidak boleh menggunakan operator **
+#          Masukan b akan terbatas pada bilangan bulat
+def power(a, b):
+    # Kode Anda di sini
+    return
+
+# E. word_percentage
+# Diberikan sebuah string s berupa kalimat dan sebuah kata w
+# hitung jumlah kata w di dalam string s, lalu bagi dengan jumlah kata dalam s
+# Contoh: word_percentage('saya mau makan', 'saya') -> 1/3
+#         word_percentage('dia bilang dia suka lari', 'dia') -> 2/5
+def word_percentage(s, w):
+    # Kode Anda di sini
+    return
