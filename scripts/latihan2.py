ff600567ce89f622fd16948706b26ce91eefe9da
Ali Akbar Septiandri <mail.septiandri@gmail.com>
2017-12-03 16:16:01 +0700

    Latihan 2

---
+++ scripts/latihan2.py
@@ -0,0 +1,14 @@
+# 1. store_credit
+# https://code.google.com/codejam/contest/351101/dashboard#s=p0
+def store_credit(credit, items):
+    return
+
+# 2. reverse_words
+# https://code.google.com/codejam/contest/351101/dashboard#s=p1
+def reverse_words(s):
+    return
+
+# 3. t9_spelling
+# https://code.google.com/codejam/contest/351101/dashboard#s=p2
+def t9_spelling(message):
+    return
